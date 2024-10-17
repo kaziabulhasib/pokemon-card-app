7d5770ebb507eda0f998eb61e1cf069e219911a5
kaziabulhasib <hasibkazi420@gmail.com>
2024-10-17 14:39:06 +0530

    getting individual pokemon data

--- src/Pokemon.jsx
+++ src/Pokemon.jsx
@@ -8,6 +8,16 @@ const Pokemon = () => {
       const res = await fetch(API);
       const data = await res.json();
       console.log(data);
+
+      const detailedPokemonData = data.results.map(async (curPokemon) => {
+        const res = await fetch(curPokemon.url);
+        const data = await res.json();
+        // console.log(data);
+        return data;
+      });
+
+      const detailedResponses = await Promise.all(detailedPokemonData);
+      console.log(detailedResponses);
     } catch (error) {
       console.log(error);
     }
