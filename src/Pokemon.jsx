d50de7166d3a493b0656b9452472e71dc8b7411b
kaziabulhasib <hasibkazi420@gmail.com>
2024-10-17 15:15:00 +0530

    set Loading message & Error message

--- src/Pokemon.jsx
+++ src/Pokemon.jsx
@@ -1,13 +1,17 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "./index.css";
+import PokemonCard from "./PokemonCard";
 
 const Pokemon = () => {
+  const [pokemon, setPokemon] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
   const API = "https://pokeapi.co/api/v2/pokemon?limit=24";
   const fetchPokemon = async () => {
     try {
       const res = await fetch(API);
       const data = await res.json();
-      console.log(data);
+      // console.log(data);
 
       const detailedPokemonData = data.results.map(async (curPokemon) => {
         const res = await fetch(curPokemon.url);
@@ -18,16 +22,43 @@ const Pokemon = () => {
 
       const detailedResponses = await Promise.all(detailedPokemonData);
       console.log(detailedResponses);
+      setPokemon(detailedResponses);
+      setLoading(false);
     } catch (error) {
       console.log(error);
+      setLoading(false);
+      setError(error);
     }
   };
+
   useEffect(() => {
     fetchPokemon();
   }, []);
+
+  if (loading) {
+    return (
+      <div>
+        <h1>Loading ...........</h1>
+      </div>
+    );
+  }
+  if (error) {
+    return (
+      <div>
+        <h1>{error.message}</h1>
+      </div>
+    );
+  }
   return (
     <div>
       <h1>Hello pokemon</h1>
+      <div>
+        <ul className='cards'>
+          {pokemon.map((carPokemon) => {
+            return <PokemonCard key={carPokemon.id} pokemonData={carPokemon} />;
+          })}
+        </ul>
+      </div>
     </div>
   );
 };
---
+++ src/PokemonCard.jsx
@@ -0,0 +1,11 @@
+const PokemonCard = ({ pokemonData }) => {
+  return (
+    <li className='pokemon-card'>
+      <figure>
+        <img src='' alt='' />
+      </figure>
+    </li>
+  );
+};
+
+export default PokemonCard;
